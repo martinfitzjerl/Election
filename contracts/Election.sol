31488abf875c282c9726b7c509486fdd4dc1ba6b
Samuel Baker <Samuel.Baker9@outlook.com>
2018-11-30 22:37:45 -0500

    updated Election.sol and app.js

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,5 +1,5 @@
 //solium-disable linebreak-style
-pragma solidity >=0.4.22 <0.6.0; 
+pragma solidity >=0.4.22 <0.6.0;
 
 contract Election {
     //Model a Candidate
@@ -13,13 +13,23 @@ contract Election {
     string public candidate;
     //state variable assigned getter function
 
+    // Store accounts that have voted
+    //---I don't know where this was added in the video??
+    mapping(address => bool) public voters;
+
     //Store Candidates
     //Fetch Candidates
     mapping(uint => Candidate) public candidates;
-    //mapping is a key value pair, key is the uint and value is the Candidate
+    //mapping is a key value pair, key is the uint and value is the Candidate, cannot know how many candidates or values in mapping (returns default value) so we use a counter cache
     //Store Candidates Count
     uint public candidatesCount;
     //counter cache to fetch from mapping, state variable
+
+    // voted event
+    //---I don't know where this was added in the video??
+    event votedEvent(
+        uint indexed _candidateId
+    );
 
     // Constructor
     constructor() public{
@@ -27,8 +37,8 @@ contract Election {
         addCandidate("Candidate 2");
     }
 
-    function addCandidate(string _name) private {
-        candidatesCount ++;
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
